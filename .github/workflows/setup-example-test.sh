038996814e9031b9c97921f30cc17d3f35541460
Krzysztof Kaczor <krzkaczor@gmail.com>
2026-01-14 22:04:09 +0100

    Fix?

--- .github/workflows/setup-example-test.sh
+++ .github/workflows/setup-example-test.sh
@@ -6,7 +6,7 @@ INSTALL_CMD="${1:-bun install}"
 cd packages/superlib && bun install && bun run build && npm pack
 cd -
 
-TARBALL=$(ls packages/superlib/superlib-*.tgz)
+TARBALL=$(ls packages/superlib/*superlib-*.tgz)
 cp -r packages/examples /tmp/example-test
 cd /tmp/example-test
 sed -i 's|"superlib": "workspace:\*"|"superlib": "file:'"$GITHUB_WORKSPACE"'/'"$TARBALL"'"|' package.json
